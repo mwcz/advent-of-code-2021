7c1a563d9a10ea70cded90bb3011f5e8283e04f2
mwcz <mwc@clayto.com>
2021-12-07 15:19:57 -0500

    day 3.2 partial

--- day3/src/part2.rs
+++ day3/src/part2.rs
@@ -1,15 +1,10 @@
 #![feature(stdin_forwarders)]
-use std::io::{self, Error};
+use std::io;
 
-fn main() {
-    let lines: Vec<i64> = io::stdin()
-        .lines()
-        .map(|bits| i64::from_str_radix(&bits.as_ref().unwrap(), 2).unwrap())
-        .collect();
-
+fn bit_hist(bits: &Vec<i64>, tie: i64) -> [i64; 12] {
     let mut bit_hist = [0; 12];
 
-    for line in &lines {
+    for line in bits.iter() {
         bit_hist[0] += -1 + 2 * ((line & 0b100000000000) >> 11);
         bit_hist[1] += -1 + 2 * ((line & 0b010000000000) >> 10);
         bit_hist[2] += -1 + 2 * ((line & 0b001000000000) >> 9);
@@ -24,38 +19,63 @@ fn main() {
         bit_hist[11] += -1 + 2 * (line & 0b000000000001);
     }
 
-    let popular_bits = bit_hist.map(|b: i64| ((b.abs() / b) + 1) / 2);
+    let popular_bits = bit_hist.map(|b: i64| if b > 0  { ((b.abs() / b) + 1) / 2 } else { tie });
+    
+    popular_bits
+}
 
-    let bit_string = popular_bits
-        .map(|b: i64| b as u64)
-        .map(|b: u64| b.to_string())
-        .join("");
+fn main() {
+    let lines: Vec<i64> = io::stdin()
+        .lines()
+        .map(|bits| i64::from_str_radix(&bits.as_ref().unwrap(), 2).unwrap())
+        .collect();
 
-    println!("bit_hist {:?}", bit_hist);
-    println!("bit_string {}", bit_string);
+    let popular_bits = bit_hist(&lines, 1);
 
-    let mut lines: Vec<i64> = lines;
+    let parts = lines
+        .iter()
+        .partition(|bits| ((*bits & (0b100000000000)) >> 11) == popular_bits[0]);
 
-    for i in (0..12).rev() {
+    let mut ox: Vec<i64> = parts.0;
+    let mut co2: Vec<i64> = parts.1;
 
-        lines = lines
-            .iter()
-            .filter(|bits| ((*bits & (0b000000000001 << i)) >> i) == popular_bits[i])
-            .map(|bits| *bits)
-            .collect();
+    for i in (0..11).rev() {
 
-        if lines.len() == 1 {
-            break;
+        let mask = 0b000000000001 << i;
+
+        if ox.len() > 1 {
+            let popular_bits = bit_hist(&ox, 1);
+            ox = ox
+                .iter()
+                .filter(|bits| ((*bits & mask) >> i) == popular_bits[11-i])
+                .map(|bits| *bits)
+                .collect();
         }
+
+        if co2.len() > 1 {
+            let popular_bits = bit_hist(&co2, 0);
+            println!("co2 pop bits: {:?}", popular_bits);
+            println!("  i: {}", i);
+            println!("  bit mask:    {:#014b}", mask);
+            
+            co2 = co2
+                .iter()
+                .filter(|bits| {
+                    println!("  pop bit:     {}", popular_bits[11-i]);
+                    println!("  bits:        {:#014b}", bits);
+                    println!("  filter pass: {}", ((*bits & mask) >> i) != popular_bits[11-i]);
+                    
+                    ((*bits & mask) >> i) != popular_bits[11-i]
+                })
+                .map(|bits| *bits)
+                .collect();
+        }
+
+        println!("ox: {:?}\tco2: {}", ox.len(), co2.len());
 
     }
 
-    println!("lines {:?}", lines);
-
-    let gamma = u64::from_str_radix(&bit_string, 2).unwrap();
-    let epsilon = gamma ^ 0b111111111111; // flip the lower 12 bits
-
-    println!("gamma: {}", gamma);
-    println!("epsilon: {}", epsilon);
-    println!("gamma * epsilon: {}", epsilon * gamma);
+    println!("ox: {}", ox.get(0).unwrap());
+    println!("co2: {}", co2.get(0).unwrap());
+    println!("ox * co2: {}", ox.get(0).unwrap() * co2.get(0).unwrap());
 }
