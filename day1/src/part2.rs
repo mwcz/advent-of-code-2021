7ac45156ed45cce2632b3de08733e14d50590b95
mwcz <mwc@clayto.com>
2021-12-06 23:08:59 -0500

    initial commit, with days 1.1, 1.2, 2.1, 2.2, 3.1, 6.1, 6.2

---
+++ day1/src/part2.rs
@@ -0,0 +1,28 @@
+#![feature(stdin_forwarders)]
+use std::io::{self, Error, Lines, StdinLock};
+
+fn to_int(line: &Result<String, Error>) -> i64 {
+    match line {
+        Ok(ref s) => s.parse::<i64>().unwrap(),
+        Err(..) => 0,
+    }
+}
+
+fn main() {
+    let lines: Vec<Result<String, Error>> = io::stdin().lines().collect();
+
+    let mut incs = 0;
+    let mut last = i64::MAX;
+
+    for line in lines.windows(3) {
+        let sum: i64 = line.iter().map(to_int).sum();
+
+        if sum > last {
+            incs += 1;
+        }
+
+        last = sum;
+    }
+
+    println!("Increments: {}", incs);
+}
---
+++ day3/src/part2.rs
@@ -0,0 +1,69 @@
+#![feature(stdin_forwarders)]
+use std::io::{self, Error};
+
+fn main() {
+    let lines: Vec<i64> = io::stdin()
+        .lines()
+        .map(|bits| i64::from_str_radix(&bits.as_ref().unwrap(), 2).unwrap())
+        .collect();
+
+    let mut bit_hist = [0; 12];
+
+    for line in &lines {
+        bit_hist[0] += -1 + 2 * ((line & 0b100000000000) >> 11);
+        bit_hist[1] += -1 + 2 * ((line & 0b010000000000) >> 10);
+        bit_hist[2] += -1 + 2 * ((line & 0b001000000000) >> 9);
+        bit_hist[3] += -1 + 2 * ((line & 0b000100000000) >> 8);
+        bit_hist[4] += -1 + 2 * ((line & 0b000010000000) >> 7);
+        bit_hist[5] += -1 + 2 * ((line & 0b000001000000) >> 6);
+        bit_hist[6] += -1 + 2 * ((line & 0b000000100000) >> 5);
+        bit_hist[7] += -1 + 2 * ((line & 0b000000010000) >> 4);
+        bit_hist[8] += -1 + 2 * ((line & 0b000000001000) >> 3);
+        bit_hist[9] += -1 + 2 * ((line & 0b000000000100) >> 2);
+        bit_hist[10] += -1 + 2 * ((line & 0b000000000010) >> 1);
+        bit_hist[11] += -1 + 2 * (line & 0b000000000001);
+    }
+
+    let popular_bits = bit_hist.map(|b: i64| ((b.abs() / b) + 1) / 2);
+
+    let bit_string = popular_bits
+        .map(|b: i64| b as u64)
+        .map(|b: u64| b.to_string())
+        .join("");
+
+    println!("bit_hist {:?}", bit_hist);
+    println!("bit_string {}", bit_string);
+
+    let lines: Vec<&i64> = lines
+        .iter()
+        .filter(|bits| {
+            // println!("bits: {}", bits);
+            ((*bits & 0b100000000000) >> 11) == popular_bits[0]
+        })
+        .collect();
+
+    let lines: Vec<&&i64> = lines
+        .iter()
+        .filter(|bits| {
+            // println!("bits: {}", bits);
+            ((**bits & 0b010000000000) >> 10) == popular_bits[1]
+        })
+        .collect();
+
+    let lines: Vec<&&&i64> = lines
+        .iter()
+        .filter(|bits| {
+            // println!("bits: {}", bits);
+            ((***bits & 0b001000000000) >> 9) == popular_bits[2]
+        })
+        .collect();
+
+    println!("lines {:?}", lines);
+
+    let gamma = u64::from_str_radix(&bit_string, 2).unwrap();
+    let epsilon = gamma ^ 0b111111111111; // flip the lower 12 bits
+
+    println!("gamma: {}", gamma);
+    println!("epsilon: {}", epsilon);
+    println!("gamma * epsilon: {}", epsilon * gamma);
+}
