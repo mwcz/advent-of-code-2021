700a9259213387be9074a3f11a36bf1a1d8abb03
mwcz <mwc@clayto.com>
2022-07-19 09:55:39 -0400

    a little day 1 cleanup

--- day1/src/part2.rs
+++ day1/src/part2.rs
@@ -1,14 +1,14 @@
-use std::io::{self, Error, Lines, StdinLock};
+use std::io;
 
-fn to_int(line: &Result<String, Error>) -> i64 {
+fn to_int(line: &Result<String, io::Error>) -> i64 {
     match line {
         Ok(ref s) => s.parse::<i64>().unwrap(),
-        Err(..) => 0,
+        Err(_) => 0,
     }
 }
 
 fn main() {
-    let lines: Vec<Result<String, Error>> = io::stdin().lines().collect();
+    let lines: Vec<Result<String, io::Error>> = io::stdin().lines().collect();
 
     let mut incs = 0;
     let mut last = i64::MAX;
